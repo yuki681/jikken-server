4f87e97ac5ca3c40ed6f7f258f3fb2ef4e73087c
Koike Takefumi <takefumic@gmail.com>
2019-06-19 09:26:23 +0900

    migration

---
+++ database/migrations/2019_06_12_024035_create_menus_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->increments('id')->primary();
+            $table->string('name', 100);
+            $table->integer('price');
+            $table ->decimal('energy',5,1)
+            $table ->decimal('protein',4,1)
+            $table ->decimal('lipid',4,1)
+            $table ->decimal('salt',3,1)
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
