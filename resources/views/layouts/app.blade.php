05bb9837bc401b12f3f00ba8d06ff80ac5ca216c
TakuyaRobo <mc22167ot@gmail.com>
2019-07-10 10:52:30 +0900

    デザイン調整

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,11 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-
+        
+        <!-- Add icon library -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">  
+        <!--満足度の星--> 
+        <link rel="stylesheet" href="{{asset('css/style.css')}}"> 
 
         <script>
         $( function() {
